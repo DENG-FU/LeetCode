72103e86713a80bd6eeb7e5ae3ae96b22c663845
DENGFU <dengfu123@live.com>
2016-08-02 00:00:04 +0800

    ReverseString function

---
+++ src/leetcode/ReverseString.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Please input a string:");
+		Scanner scan = new Scanner(System.in);
+		String string = scan.nextLine();
+		String reverse = reverseString(string);
+		System.out.println("The reversed result:"+reverse);
+	}
+	
+	public static String reverseString(String s){
+		if (s.length() == 1){
+			return s;
+		} else {
+			String left = s.substring(0, s.length()/2);
+			String right = s.substring(s.length()/2, s.length());
+			return reverseString(right)+reverseString(left);
+		}
+	}
+
+}
